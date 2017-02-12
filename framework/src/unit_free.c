bcc8f26697bb4624aa1f749bbf8c9ae3d8fd31a9
mtassett <marc.tassetti@gmail.com>
2017-02-12 02:34:29 +0100

    Added unit_free.c, free the t_unit list

---
+++ framework/src/unit_free.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unit_free.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtassett <mtassett@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/11 02:09:34 by mtassett          #+#    #+#             */
+/*   Updated: 2017/02/11 02:15:25 by mtassett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/libunit.h"
+#include <stdint.h>
+
+void	unit_free(t_list *unit)
+{
+	t_unit	*curr;
+	t_unit	*prev;
+
+	curr = unit;
+	prev = NULL;
+	//TODO: finish it
+}
